537f999988c2955c9cc121e16ff513a5601b967a
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-06-30 12:27:42 -0700

    website beta 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React from 'react';
 import './App.scss';
+
+import hoaVietMarketImage from "./assets/hoa_viet_market.png"
+import symbolicRegressorImage from "./assets/symbolic_regressor.png";
+import paydayPurgatoryImage from "./assets/payday_purgatory.png";
+import projectilePoopersImage from "./assets/projectile_poopers.png";
 
 function App() {
   const handleOpenFile = () => {
@@ -34,9 +37,12 @@ function App() {
         <h4>Hi, my name is</h4>
         <h1>Eric Ly.</h1>
         <h2>I'm a Software Engineer. </h2>
-        <p id="introduction-paragraph">Currently, I'm studying Computer Science & Economics, at California State University, Fullerton. </p>
-        <p>This is my personal website, where I highlight my projects and interests. </p>
-        <p>Thanks for reading!  👻 </p>
+        <div className="introduction-paragraph">
+          <p>Currently, I'm studying Computer Science & Economics, at California State University, Fullerton. </p>
+          <p>This is my personal website, where I highlight my projects and interests. </p>
+          <p>Thanks for reading!  👻 </p>
+        </div>
+
       </div>
 
 
@@ -47,47 +53,49 @@ function App() {
         <button onClick={handleSendEmail}>Email</button>
       </div>
 
-      <div className="projects-container">
+      <div className="section-container">
         <h1 id="projects-subtitle">Projects</h1>
-        <div className="card project-card">
-          <img src="./src/assets/symbolic_regressor.png" alt="Project 3" className="project-image" />
-          <h3>Symbolic Regressor for Pringles</h3>
-          <p>
-            &emsp;&emsp;During my ASSURE-US research program, I explored genetic programming and developed a usable fitness model for Symbolic Regressor.
-            Given 50 data points that maps a hyperbolic paraboloid, the model was able to reveal the formula of such shape.
-            BTS, the Symbolic Regressor is utilizing selections, crossovers, and mutations, to derive the fittest genetic programs, over multiple generations.
-          </p>
-        </div>
-        <div className="card project-card">
-          <img src="./src/assets/payday_purgatory.png" alt="Project 1" className="project-image" />
-          <h3>Payday Purgatory</h3>
-          <p>
-            &emsp;&emsp;Each round, players contribute a portion of their total bank balance.
-            The player with the lowest contributions that round, loses a heart.
-            If all hearts are depleted, the player is eliminated.
-            The objective is be the last contributor standing.
-            Players are authenticated through Firebase authenticator, and data is stored/modified on Firebase's database.
-          </p>
-        </div>
-        <div className="card project-card">
-          <img src="./src/assets/projectile_poopers.png" alt="Project 2" className="project-image" />
-          <h3>Projectile Poopers</h3>
-          <p>
-            &emsp;&emsp;I built this from Drew Conley's multiplayer JS game.
-            I introduced game music and coin sounds, and developed button-based controls for mobile devices.
-            I added hearts to eliminate players who go below 0, and designed/implemented projectiles that are able to shoot out and damage players.
-            I created a coins market, where players are able to redeem their coins for HP and projectile damage, and utilized Firebase to authenticate, reference, and update players' & objects' data. 
-          </p>
-        </div>
-        <div className="card project-card">
-          <img src="./src/assets/hoa_viet_market.png" alt="Project 3" className="project-image" />
-          <h3>Hoa Viet Market Business Website</h3>
-          <p>
-            &emsp;&emsp;I develop a website for a small, Vietnamese business in Los Angeles, and passed as a finalist for Snapchat Academies' engineering assessment.
-            The website features contact pages, operating hours, customer accommodations, a photos photo-gallery, and Google maps integrations.
-          </p>
+        <div className="projects-container">
+          <div className="card project-card">
+            <img src={symbolicRegressorImage} alt="Project 3" className="project-image" />
+            <h3>Symbolic Regressor for Pringles</h3>
+            <p>
+              &emsp;&emsp;During my ASSURE-US research program, I explored genetic programming and developed a usable fitness model for Symbolic Regressor.
+              Given 50 data points that maps a hyperbolic paraboloid, the model was able to reveal the formula of such shape.
+              BTS, the Symbolic Regressor is utilizing selections, crossovers, and mutations, to derive the fittest genetic programs, over multiple generations.
+            </p>
+          </div>
+          <div className="card project-card">
+            <img src={paydayPurgatoryImage} alt="Project 1" className="project-image" />
+            <h3>Payday Purgatory</h3>
+            <p>
+              &emsp;&emsp;Each round, players contribute a portion of their total bank balance.
+              The player with the lowest contributions that round, loses a heart.
+              If all hearts are depleted, the player is eliminated.
+              The objective is be the last contributor standing.
+              Players are authenticated through Firebase authenticator, and data is stored/modified on Firebase's database.
+            </p>
+          </div>
+          <div className="card project-card">
+            <img src={projectilePoopersImage} alt="Project 2" className="project-image" />
+            <h3>Projectile Poopers</h3>
+            <p>
+              &emsp;&emsp;Built upon Drew Conley's multiplayer JS game— adding game music, coin sounds, and mobile-friendly button-based controls. 
+              Implemented an HP system to eliminate players falling below 0 HP and designed projectiles for player damage. 
+              Created a coins market where players can redeem coins for HP and projectile upgrades— utilizing Firebase for authentication and data management.
+            </p>
+          </div>
+          <div className="card project-card">
+            <img src={hoaVietMarketImage} alt="Project 3" className="project-image" />
+            <h3>Hoa Viet Market Business Website</h3>
+            <p>
+              &emsp;&emsp;I develop a website for a small, Vietnamese business in Los Angeles, and passed as a finalist for Snapchat Academies' engineering assessment.
+              The website features contact pages, operating hours, customer accommodations, a photos photo-gallery, and Google maps integrations.
+            </p>
+          </div>
         </div>
       </div>
+
     </>
   );
 }
