01149b34611b17d5a8a7ef38f1d33a7205234f98
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-06-30 12:43:09 -0700

    website beta 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -60,16 +60,18 @@ function App() {
             <img src={symbolicRegressorImage} alt="Project 3" className="project-image" />
             <h3>Symbolic Regressor for Pringles</h3>
             <p>
-              &emsp;&emsp;During my ASSURE-US research program, I explored genetic programming and developed a usable fitness model for Symbolic Regressor.
+              During my ASSURE-US research program, I explored genetic programming and developed a usable fitness model for Symbolic Regressor.
               Given 50 data points that maps a hyperbolic paraboloid, the model was able to reveal the formula of such shape.
               BTS, the Symbolic Regressor is utilizing selections, crossovers, and mutations, to derive the fittest genetic programs, over multiple generations.
             </p>
           </div>
           <div className="card project-card">
-            <img src={paydayPurgatoryImage} alt="Project 1" className="project-image" />
+            <a href="https://www.payday-purgatory.lyyeric.tech/" target="_blank" rel="noopener noreferrer">
+              <img src={paydayPurgatoryImage} alt="Project 1" className="project-image" />
+            </a>
             <h3>Payday Purgatory</h3>
             <p>
-              &emsp;&emsp;Each round, players contribute a portion of their total bank balance.
+              Each round, players contribute a portion of their total bank balance.
               The player with the lowest contributions that round, loses a heart.
               If all hearts are depleted, the player is eliminated.
               The objective is be the last contributor standing.
@@ -77,19 +79,23 @@ function App() {
             </p>
           </div>
           <div className="card project-card">
-            <img src={projectilePoopersImage} alt="Project 2" className="project-image" />
+            <a href="https://www.projectile-poopers.lyyeric.tech/" target="_blank" rel="noopener noreferrer">
+              <img src={projectilePoopersImage} alt="Project 2" className="project-image" />
+            </a>
             <h3>Projectile Poopers</h3>
             <p>
-              &emsp;&emsp;Built upon Drew Conley's multiplayer JS game— adding game music, coin sounds, and mobile-friendly button-based controls. 
-              Implemented an HP system to eliminate players falling below 0 HP and designed projectiles for player damage. 
+              Built upon Drew Conley's multiplayer JS game— adding game music, coin sounds, and mobile-friendly button-based controls.
+              Implemented an HP system to eliminate players falling below 0 HP and designed projectiles for player damage.
               Created a coins market where players can redeem coins for HP and projectile upgrades— utilizing Firebase for authentication and data management.
             </p>
           </div>
           <div className="card project-card">
-            <img src={hoaVietMarketImage} alt="Project 3" className="project-image" />
+            <a href="https://www.hoa-viet-market.lyyeric.tech/" target="_blank" rel="noopener noreferrer">
+              <img src={hoaVietMarketImage} alt="Project 3" className="project-image" />
+            </a>
             <h3>Hoa Viet Market Business Website</h3>
             <p>
-              &emsp;&emsp;I develop a website for a small, Vietnamese business in Los Angeles, and passed as a finalist for Snapchat Academies' engineering assessment.
+              I develop a website for a small, Vietnamese business in Los Angeles, and passed as a finalist for Snapchat Academies' engineering assessment.
               The website features contact pages, operating hours, customer accommodations, a photos photo-gallery, and Google maps integrations.
             </p>
           </div>
