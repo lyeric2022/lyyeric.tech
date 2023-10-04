323aa37b16ae65a9d96dcbcfed89f060e087c249
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-10-03 18:01:34 -0700

    content for video media

--- src/VideoMedia.jsx
+++ src/VideoMedia.jsx
@@ -6,8 +6,48 @@ const VideoMedia = () => {
     return (
         <div className="section-container">
             <h1 id="projects-subtitle">Video Media</h1>
-            <div className="video-media-container">
-                
+            <div className="projects-container">
+
+                <div className="card video-media-card">
+                    <a>
+                        <img src={"https://cdn.myanimelist.net/images/anime/1171/109222.jpg"} alt="Project 3" className="project-image" />
+                    </a>
+                    <h3>Jujutsu Kaisen</h3>
+                    <p>
+                    </p>
+                </div>
+                <div className="card video-media-card">
+                    <a>
+                        <img src={"https://cdn.myanimelist.net/images/anime/1009/98400l.jpg?_gl=1*1aocgio*_ga*MTIzMzAwMjQ5My4xNjk2Mzc4NDc5*_ga_26FEP9527K*MTY5NjM3ODQ4MS4xLjEuMTY5NjM3ODQ4OS41Mi4wLjA."} alt="Project 3" className="project-image" />
+                    </a>
+                    <h3>Noragami</h3>
+                    <p>
+                    </p>
+                </div>
+                <div className="card video-media-card">
+                    <a>
+                        <img src={"https://bubbawheat.files.wordpress.com/2014/11/big-hero-6.jpg"} alt="Project 3" className="project-image" />
+                    </a>
+                    <h3>Big Hero 6</h3>
+                    <p>
+                    </p>
+                </div>
+                <div className="card video-media-card">
+                    <a>
+                        <img src={"https://cdn.myanimelist.net/images/anime/6/87709.jpg"} alt="Project 3" className="project-image" />
+                    </a>
+                    <h3>Code Geass</h3>
+                    <p>
+                    </p>
+                </div>
+                <div className="card video-media-card">
+                    <a>
+                        <img src={"https://cdn.myanimelist.net/images/anime/1009/98400l.jpg?_gl=1*1aocgio*_ga*MTIzMzAwMjQ5My4xNjk2Mzc4NDc5*_ga_26FEP9527K*MTY5NjM3ODQ4MS4xLjEuMTY5NjM3ODQ4OS41Mi4wLjA."} alt="Project 3" className="project-image" />
+                    </a>
+                    <h3>Noragami</h3>
+                    <p>
+                    </p>
+                </div>
             </div>
         </div>
     );
