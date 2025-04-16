2f6016b171e39d953de5b958e19b09449eb53e9d
Eric Ly <ly.eric2022@csu.fullerton.edu>
2025-04-16 12:25:01 -0700

    new firebase configs

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDey67QmxKQl4AuwLGpFT3UeXTtk1z6R94",
-    authDomain: "lyyeric-d727f.firebaseapp.com",
-    projectId: "lyyeric-d727f",
-    storageBucket: "lyyeric-d727f.firebasestorage.app",
-    messagingSenderId: "335055775316",
-    appId: "1:335055775316:web:c9360aae04a48abfd9efc1",
-    measurementId: "G-1JRGXLD88S"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
